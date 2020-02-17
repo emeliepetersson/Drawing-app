df6399636950ab2e120789068c3dd91d4d5d057f
emeliepetersson <emepet1204@skola.goteborg.se>
2020-02-17 17:36:07 +0100

    disable scrolling

--- scripts/touch-events.js
+++ scripts/touch-events.js
@@ -23,3 +23,16 @@ function touchStarted() {
       break;
   }
 }
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function disableScroll() {
+  document.body.addEventListener("touchmove", preventDefault, {
+    passive: false
+  });
+}
+function enableScroll() {
+  document.body.removeEventListener("touchmove", preventDefault);
+}
