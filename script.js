66a5f828e6b6c948c9992d0f8b83d89b2235d34a
emeliepetersson <emepet1204@skola.goteborg.se>
2020-02-17 11:23:35 +0100

    add p5.js and create canvas

---
+++ script.js
@@ -0,0 +1,11 @@
+"user strict";
+
+function setup() {}
+
+function draw() {
+  // create a canvas which is full width and height
+  createCanvas(window.innerWidth, window.innerHeight);
+  ellipse(50, 50, 80, 80);
+  // Add a white background to the canvas
+  background(255);
+}
