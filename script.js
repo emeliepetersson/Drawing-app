8345f14080112657ca0170bcc4e2d9683dff9476
emeliepetersson <emepet1204@skola.goteborg.se>
2020-02-17 12:53:04 +0100

    add square shape

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const colorInput = document.getElementById("color");
 const weight = document.getElementById("weight");
 const shape = document.getElementById("shape");
 const clear = document.getElementById("clear");
+
+// Arrays to store the different paths in, based on the shape
 const strokePaths = [];
 const ellipsePaths = [];
+const squarePaths = [];
 let currentStrokePath = [];
 let currentEllipsePath = [];
+let currentSquarePath = [];
 
 function setup() {
   // create a canvas which is full width and height
@@ -34,10 +38,13 @@ function draw() {
       case "ellipse":
         currentEllipsePath.push(point);
         break;
+      case "square":
+        currentSquarePath.push(point);
+        break;
     }
   }
 
-  // Looping over all the paths and drawing all the points inside them
+  // Looping over all the paths and drawing all the shapes inside them
   strokePaths.forEach(path => {
     beginShape();
     path.forEach(point => {
@@ -57,12 +64,26 @@ function draw() {
     });
     endShape();
   });
+
+  squarePaths.forEach(path => {
+    beginShape();
+    path.forEach(point => {
+      stroke(point.color);
+      strokeWeight(point.weight);
+      vertex(point.x, point.y - 10);
+      vertex(point.x + 55, point.y - 10);
+      vertex(point.x + 55, point.y + 45);
+      vertex(point.x, point.y + 45);
+    });
+    endShape();
+  });
 }
 
 function mousePressed() {
   // Clean up the currentPath
   currentStrokePath = [];
   currentEllipsePath = [];
+  currentSquarePath = [];
 
   // Push the new path inside the paths array
   switch (shape.value) {
@@ -72,12 +93,16 @@ function mousePressed() {
     case "ellipse":
       ellipsePaths.push(currentEllipsePath);
       break;
+    case "square":
+      squarePaths.push(currentSquarePath);
+      break;
   }
 }
 
 clear.addEventListener("click", () => {
   // Remove all the paths
-  paths.splice(0);
+  ellipsePaths.splice(0);
+  strokePaths.splice(0);
 
   // Clear the background
   background(255);
