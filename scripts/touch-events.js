a347d40f513dc82715f49e6d88d25184562637c5
emeliepetersson <emepet1204@skola.goteborg.se>
2020-02-17 17:41:12 +0100

    remove disable scroll in js and update styling with css

--- scripts/touch-events.js
+++ scripts/touch-events.js
@@ -23,19 +23,3 @@ function touchStarted() {
       break;
   }
 }
-
-var xStart,
-  yStart = 0;
-
-document.addEventListener("touchstart", function(e) {
-  xStart = e.touches[0].screenX;
-  yStart = e.touches[0].screenY;
-});
-
-document.addEventListener("touchmove", function(e) {
-  var xMovement = Math.abs(e.touches[0].screenX - xStart);
-  var yMovement = Math.abs(e.touches[0].screenY - yStart);
-  if (yMovement * 3 > xMovement) {
-    e.preventDefault();
-  }
-});
